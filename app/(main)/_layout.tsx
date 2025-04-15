94d077020bf83216c4db72b3a72850424630639c
damanjot-devronins <damanjotdevronins@gmail.com>
2025-04-15 10:33:21 +0530

    fix: remove location permission logic fron home screen

--- app/(main)/_layout.tsx
+++ app/(main)/_layout.tsx
@@ -1,33 +1,6 @@
 import { Stack } from "expo-router";
-import { PermissionTypeEnum } from "@/reducers";
-import { useAppDispatch, useTypedSelector } from "@/srore";
-import { useEffect, useRef } from "react";
-import { requestBackgroundLocationAccess } from "@/services/permission";
-import { AppState } from "react-native";
 
 const Layout = () => {
-  const dispatch = useAppDispatch();
-  const { location } = useTypedSelector(state => state.Permission);
-  const appState = useRef(AppState.currentState);
-
-  useEffect(() => {
-    // First time request if undetermined
-    if (location.grantedBackground === PermissionTypeEnum.UNDETERMINED) {
-      dispatch(requestBackgroundLocationAccess());
-    }
-
-    const subscription = AppState.addEventListener("change", nextAppState => {
-      if (appState.current.match(/inactive|background/) && nextAppState === "active") {
-        console.log("📲 App returned to foreground, rechecking permission...");
-        dispatch(requestBackgroundLocationAccess());
-      }
-
-      appState.current = nextAppState;
-    });
-
-    return () => subscription.remove();
-  }, []);
-
   return (
     <Stack
       screenOptions={{
