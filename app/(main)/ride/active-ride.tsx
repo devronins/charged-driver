6def9056bf60ebf090d5ad3cfb54c2eb18d75a9f
damanjot-devronins <damanjotdevronins@gmail.com>
2025-05-19 16:51:00 +0530

    fix: fix bugs

--- app/(main)/ride/active-ride.tsx
+++ app/(main)/ride/active-ride.tsx
@@ -65,7 +65,7 @@ const ActiveRide = () => {
         })
       );
     }
-  }, [activeRide?.status]);
+  }, []);
 
   useEffect(() => {
     if (activeRide?.status === RideStatus.Started || activeRide?.status === RideStatus.Accepted) {
